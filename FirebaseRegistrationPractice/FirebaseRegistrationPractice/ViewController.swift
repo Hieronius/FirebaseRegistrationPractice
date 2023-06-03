6833df88fe7e3c2d82e133298c6050967985c871
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-03 10:34:18 +0300

    Add outlets for ViewController

--- FirebaseRegistrationPractice/FirebaseRegistrationPractice/ViewController.swift
+++ FirebaseRegistrationPractice/FirebaseRegistrationPractice/ViewController.swift
@@ -9,7 +9,32 @@ import UIKit
 import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    // MARK: Registration Block
+    
+    @IBOutlet weak var registrationEmailTextField: UITextField!
+    @IBOutlet weak var registrationPasswordTextField: UITextField!
+    @IBOutlet weak var registrationRepeatPasswordTextField: UITextField!
+    
+    // MARK: Authorisation Block
+    
+    
+    @IBOutlet weak var logInEmailTextField: UITextField!
+    @IBOutlet weak var logInPasswordTextField: UITextField!
+    
+    // MARK: Reset Password Block
+    
+    @IBOutlet weak var resetPasswordEmailTextField: UITextField!
+    
+    // MARK: Check of the current user authorisation state block
+    
+    @IBOutlet weak var logOutButtonView: UIButton!
+    @IBOutlet weak var deleteAccountButtonView: UIButton!
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
