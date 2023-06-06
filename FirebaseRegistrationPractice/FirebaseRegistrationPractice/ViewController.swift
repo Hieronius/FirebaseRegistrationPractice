963ee4f62f944a85aad2f5883799569ab0e1c316
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-06 09:25:09 +0300

    Add email verification check during logging

--- FirebaseRegistrationPractice/FirebaseRegistrationPractice/ViewController.swift
+++ FirebaseRegistrationPractice/FirebaseRegistrationPractice/ViewController.swift
@@ -50,46 +50,72 @@ class ViewController: UIViewController {
     
     // MARK: - IBActions
     
+    // MARK: CREATE NEW ACCOUNT
+    
     // Creation of new account
     // Method working correctly
     @IBAction func signUpButtonAction(_ sender: UIButton) {
-        // check to prevent nil in login/pass fields
-        let email = registrationEmailTextField.text ?? ""
-        let password = registrationPasswordTextField.text ?? ""
         
-        // MARK: Create a new account
-        // create account via Firebase
-        // probably we use weak self to make a link to the current view controller you have data from
-        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+        Task {
+            // check to prevent nil in login/pass fields
+            let email = registrationEmailTextField.text ?? ""
+            let password = registrationPasswordTextField.text ?? ""
+            let repeatPassword = registrationRepeatPasswordTextField.text ?? ""
             
-            // check is there a data for account creation
-            guard let strongSelf = self else { return }
-            
-            
-            // MARK: Email verification
-            // this code is really working
-            Auth.auth().currentUser?.sendEmailVerification()
-            
-            
-            
-            // is not print a message and exit the method
-            guard error == nil else {
-                print("Account creation failed")
-                // error handling for allert controller if user trying to use email which is already exist in the system
-                if error?.localizedDescription == "The email address is already in use by another account." {
-                    print("Email already in use")
+            if password == repeatPassword {
+                
+                // create account via Firebase
+                // probably we use weak self to make a link to the current view controller you have data from
+                FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+                    
+                    // check is there a data for account creation
+                    guard let strongSelf = self else { return }
+                    
+                    
+                    // MARK: EMAIL VERIFICATION
+                    // this code is really working
+                    Auth.auth().currentUser?.sendEmailVerification()
+                    
+                    
+                    
+                    // is not print a message and exit the method
+                    guard error == nil else {
+                        print("Account creation failed")
+                        // error handling for allert controller if user trying to use email which is already exist in the system
+                        if error?.localizedDescription == "The email address is already in use by another account." {
+                            print("Email already in use")
+                        }
+                        return
+                    }
+                    // we should see this message if account has been created
+                    // 1. Add Alert controller here
+                    print("Account has been created")
+                    
+                    // MARK: Force user to logout after registration.
+                    let firebaseAuth = FirebaseAuth.Auth.auth()
+                    
+                    // implement error handling while you wan't log out. Seems like it's need because there can be nil instead of user. (First loading of the app for example)
+                    do {
+                        try firebaseAuth.signOut()
+                        print("User has been logged out after registration")
+                    } catch let signOutError as NSError {
+                        print("Error signing out: %@", signOutError)
+                    }
+                    
                 }
-                return
+            } else {
+                // 2. Add Alert controller here
+                print("Wrong password")
             }
-            // we should see this message if account has been created
-            print("Account has been created")
         }
         
         
     }
     
+    // MARK: LOGIN TO THE APP
+    
     @IBAction func logInButtonAction(_ sender: UIButton) {
-         Task {
+        Task {
             // run code asynchronously even in synchronous method of UIButton
             // It's still running in the main thread but in the background
             // Firebae guides provides info that almost all of their API is asynchronous, so i will try to use Tasks for these purposes
@@ -97,29 +123,35 @@ class ViewController: UIViewController {
             // check to find a nil or define a default values
             let email = self.logInEmailTextField.text ?? ""
             let password = self.logInPasswordTextField.text ?? ""
-            
-             FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { [weak self] resutl, error in
-                guard let result = self else { return }
                 
-                guard error == nil else {
-                    print("wrong login or password")
-                    return
-                }
-                print("Welcome to the app!")
-                self?.isUserLoggedIn.isHidden = false
-                print(FirebaseAuth.Auth.auth().currentUser)
-                print(FirebaseAuth.Auth.auth().currentUser?.email)
-                // little code about how to check is emailVerified or not
-                if Auth.auth().currentUser?.isEmailVerified == true {
+                FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { [weak self] resutl, error in
+                    guard let result = self else { return }
+                    
+                    // MARK: Check is user email verified
+                    if Auth.auth().currentUser?.isEmailVerified == true {
+                    
+                    guard error == nil else {
+                        print("wrong login or password")
+                        return
+                    }
+                    print("Welcome to the app!")
                     print("User is verified")
+                    self?.isUserLoggedIn.isHidden = false
+                    print(FirebaseAuth.Auth.auth().currentUser)
+                    print(FirebaseAuth.Auth.auth().currentUser?.email)
+                    // little code about how to check is emailVerified or not
                 } else {
+                    // 3. Alert controller - emain need to be verified
                     print("User still need to verify email")
                 }
+                    
+                // we should see if user log in was successful
+                
             }
-            // we should see if user log in was successful
-            
-         }
+        }
     }
+    
+    // MARK: RESET PASSWORD
     
     @IBAction func resetPasswordButtonAction(_ sender: UIButton) {
         
@@ -129,7 +161,7 @@ class ViewController: UIViewController {
         print("Email with a link to change your password has been send")
     }
     
-    // MARK: Should change a state of user authorisation
+    // MARK: LOGOUT FROM THE APP
     
     @IBAction func logOutButtonAction(_ sender: UIButton) {
         // define an instance of FirebaseAuthorisation module
@@ -145,6 +177,8 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    // MARK: DELETE ACCOUNT
     
     @IBAction func deleteAccoutButtonAction(_ sender: UIButton) {
         FirebaseAuth.Auth.auth().currentUser?.delete()
